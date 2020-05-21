c2bae2727bca4fabc341e0b2cdf32a15883af5b1
Eliana Miller <elianasmiller@gmail.com>
2020-05-21 13:01:16 -0400

    Updates

---
+++ precache-manifest.811c6b295560897f9449337d465929db.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a2d399c874773528b5f526dd775232d",
+    "url": "/boilermaker/index.html"
+  },
+  {
+    "revision": "60c85d02f6f6d4820720",
+    "url": "/boilermaker/static/css/main.99cbeaa3.chunk.css"
+  },
+  {
+    "revision": "7d3901894b59e74c44a1",
+    "url": "/boilermaker/static/js/2.b279ec3e.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/boilermaker/static/js/2.b279ec3e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60c85d02f6f6d4820720",
+    "url": "/boilermaker/static/js/main.db75e970.chunk.js"
+  },
+  {
+    "revision": "11aa829fdcf341811a3c",
+    "url": "/boilermaker/static/js/runtime-main.68914c4e.js"
+  },
+  {
+    "revision": "8250ad05d282739e173ea271ff59ec95",
+    "url": "/boilermaker/static/media/BreakfastSandwhich-removebg-preview.8250ad05.png"
+  },
+  {
+    "revision": "a72ae05eba8299ff5dcdff7c78b1037a",
+    "url": "/boilermaker/static/media/Recipe_take3.a72ae05e.png"
+  },
+  {
+    "revision": "38b73c275fa4415fc64ceef6e2744844",
+    "url": "/boilermaker/static/media/baconicecream-removebg-fixed.38b73c27.png"
+  },
+  {
+    "revision": "8cd86bdde8e417d118672aee93764b9c",
+    "url": "/boilermaker/static/media/beyondburger.8cd86bdd.png"
+  },
+  {
+    "revision": "87ba45c25612a4643a07c514d51c2aca",
+    "url": "/boilermaker/static/media/burger-removebg-preview_smol.87ba45c2.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/boilermaker/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "607152ace770aa20c08abf2e06155263",
+    "url": "/boilermaker/static/media/macandcheese-removebg-preview.607152ac.png"
+  },
+  {
+    "revision": "a6e76d0d97197929123a4a7799a8fb90",
+    "url": "/boilermaker/static/media/meatylasagna-removebg-fixed.a6e76d0d.png"
+  },
+  {
+    "revision": "14ba196d71052b66cfdeca0bb86e35e7",
+    "url": "/boilermaker/static/media/pizza-removebg-fixed.14ba196d.png"
+  },
+  {
+    "revision": "6e219cbf3befcde22f1c7c236b54077d",
+    "url": "/boilermaker/static/media/restuarant.6e219cbf.png"
+  }
+]);
