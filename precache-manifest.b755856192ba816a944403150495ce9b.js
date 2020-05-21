47036c6d9a3c5e21fafd5cd95a250f35a7fa7ffd
Eliana Miller <elianasmiller@gmail.com>
2020-05-21 13:19:02 -0400

    Updates

---
+++ precache-manifest.b755856192ba816a944403150495ce9b.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7283da33cfbcf640d0816adc40d7cc0c",
+    "url": "/alternaburger/index.html"
+  },
+  {
+    "revision": "cb6cd1615cb2605c244f",
+    "url": "/alternaburger/static/css/main.99cbeaa3.chunk.css"
+  },
+  {
+    "revision": "24d6869ad397d9c66230",
+    "url": "/alternaburger/static/js/2.a30123de.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/alternaburger/static/js/2.a30123de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb6cd1615cb2605c244f",
+    "url": "/alternaburger/static/js/main.fd4a8c89.chunk.js"
+  },
+  {
+    "revision": "8c01959ad6588e891631",
+    "url": "/alternaburger/static/js/runtime-main.8834c9d1.js"
+  },
+  {
+    "revision": "8250ad05d282739e173ea271ff59ec95",
+    "url": "/alternaburger/static/media/BreakfastSandwhich-removebg-preview.8250ad05.png"
+  },
+  {
+    "revision": "a72ae05eba8299ff5dcdff7c78b1037a",
+    "url": "/alternaburger/static/media/Recipe_take3.a72ae05e.png"
+  },
+  {
+    "revision": "38b73c275fa4415fc64ceef6e2744844",
+    "url": "/alternaburger/static/media/baconicecream-removebg-fixed.38b73c27.png"
+  },
+  {
+    "revision": "8cd86bdde8e417d118672aee93764b9c",
+    "url": "/alternaburger/static/media/beyondburger.8cd86bdd.png"
+  },
+  {
+    "revision": "87ba45c25612a4643a07c514d51c2aca",
+    "url": "/alternaburger/static/media/burger-removebg-preview_smol.87ba45c2.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/alternaburger/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "607152ace770aa20c08abf2e06155263",
+    "url": "/alternaburger/static/media/macandcheese-removebg-preview.607152ac.png"
+  },
+  {
+    "revision": "a6e76d0d97197929123a4a7799a8fb90",
+    "url": "/alternaburger/static/media/meatylasagna-removebg-fixed.a6e76d0d.png"
+  },
+  {
+    "revision": "14ba196d71052b66cfdeca0bb86e35e7",
+    "url": "/alternaburger/static/media/pizza-removebg-fixed.14ba196d.png"
+  },
+  {
+    "revision": "6e219cbf3befcde22f1c7c236b54077d",
+    "url": "/alternaburger/static/media/restuarant.6e219cbf.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/boilermaker/precache-manifest.811c6b295560897f9449337d465929db.js"
+  "/alternaburger/precache-manifest.b755856192ba816a944403150495ce9b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/boilermaker/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/alternaburger/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
